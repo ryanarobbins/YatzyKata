c50c3c274191038681c9e3b2bfec6af7aa5eb5b1
Ryan Robbins <rrobbinsHYL@unither.com>
2023-11-29 13:02:01 -0500

    handled number categories

--- YatzyKata/UnitTest1.cs
+++ YatzyKata/UnitTest1.cs
@@ -1,5 +1,6 @@
 namespace YatzyKata
 {
+    // using mob tool
     public class Tests
     {
         [Test]
@@ -35,6 +36,29 @@ namespace YatzyKata
         {
             var roll = new List<int> { 1, 1, 3, 1, 1 };
             Assert.That(Yatzy.Score(roll, "ONES"), Is.EqualTo(4));
+        }
+
+        [Test]
+        public void TwosAddsUpTheTwos()
+        {
+            var roll = new List<int> { 2, 1, 3, 2, 2 };
+            Assert.That(Yatzy.Score(roll, "TWOS"), Is.EqualTo(6));
+        }
+
+        [Test]
+        public void ThreesAddsUpTheThrees()
+        {
+            var roll = new List<int> { 2, 1, 3, 3, 2 };
+            Assert.That(Yatzy.Score(roll, "THREES"), Is.EqualTo(6));
+        }
+
+        [Test]
+        public void OtherNumberCategories()
+        {
+            var roll = new List<int> { 4, 4, 5, 6, 6 };
+            Assert.That(Yatzy.Score(roll, "FOURS"), Is.EqualTo(8));
+            Assert.That(Yatzy.Score(roll, "FIVES"), Is.EqualTo(5));
+            Assert.That(Yatzy.Score(roll, "SIXES"), Is.EqualTo(12));
         }
     }
 }
--- YatzyKata/Yatzy.cs
+++ YatzyKata/Yatzy.cs
@@ -2,6 +2,16 @@
 {
     public static class Yatzy
     {
+        static Dictionary<string, int> numberCategories = new()
+        {
+            {"ONES", 1 },
+            {"TWOS", 2 },
+            {"THREES", 3 },
+            {"FOURS", 4 },
+            {"FIVES", 5 },
+            {"SIXES", 6 },
+        };
+
         internal static int Score(List<int> roll, string category)
         {
             if (category == "CHANCE")
@@ -13,14 +23,20 @@
                 if (roll.Distinct().Count() == 1)
                 {
                     return 50;
-                }                
+                }
                 return 0;
             }
-            if(category == "ONES")
-            {
-                return roll.Where(x => x == 1).Sum();
-            }
+            
+            int? numberToScore = numberCategories.ContainsKey(category) ? numberCategories[category] : null;
+            if (numberToScore != null)
+                return ScoreNumberCategory(roll, numberToScore.Value);
+            
             return -1;
+        }
+
+        private static int ScoreNumberCategory(List<int> roll, int numberToScore)
+        {
+            return roll.Where(x => x == numberToScore).Sum();
         }
     }
 }
