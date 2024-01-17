bcd4656a4c14468b542ed99edce2a49ac6fc7073
Ryan Robbins <rrobbinsHYL@unither.com>
2024-01-17 12:54:16 -0500

    read in category from user

--- YatzyGameEngine/Phase.cs
+++ YatzyGameEngine/Phase.cs
@@ -6,10 +6,18 @@ namespace YatzyGameEngine
     {
         public List<int> Roll { get; set; } = default!;
 
+        public Func<string?> ReadInCategory = () => Console.ReadLine();
+
         public string RollAndPlayAndScore()
         {
-            var score = Yatzy.Score(new Play(Roll, ScoreCategory.Yatzy));
-            return $"Category: {ScoreCategory.Yatzy} Score: {score}";
+            var category = ReadInCategory();
+            
+            if (!Enum.TryParse<ScoreCategory>(category, true, out var parsedCategory))
+            {
+                return "error msg";
+            }
+            var score = Yatzy.Score(new Play(Roll, parsedCategory));
+            return $"Category: {parsedCategory} Score: {score}";
         }
     }
 }
--- YatzyKata/RollTests.cs
+++ YatzyKata/RollTests.cs
@@ -1,4 +1,5 @@
 ﻿using YatzyGameEngine;
+using YatzyScoringEngine;
 
 namespace YatzyTests
 {
@@ -7,28 +8,59 @@ namespace YatzyTests
         [Test]
         public void ScoreSuccessfulYatzy() 
         {
-
             //Call a method that will
             //present dice roll
             //take in category from user
             //return a message about the score
             var phase = new Phase();
             phase.Roll = new List<int> { 1, 1, 1, 1, 1 };
-            Assert.That(phase.RollAndPlayAndScore(), Is.EqualTo("Category: Yatzy Score: 50"));
+            var category = ScoreCategory.Yatzy.ToString();
+            phase.ReadInCategory = () => category;
+            Assert.That(phase.RollAndPlayAndScore(), Is.EqualTo($"Category: {category} Score: 50"));
         }
 
 
         [Test]
         public void ScoreFailYatzy()
         {
-
-            //Call a method that will
-            //present dice roll
-            //take in category from user
-            //return a message about the score
             var phase = new Phase();
             phase.Roll = new List<int> { 1, 1, 1, 1, 2 };
-            Assert.That(phase.RollAndPlayAndScore(), Is.EqualTo("Category: Yatzy Score: 0"));
+            var category = ScoreCategory.Yatzy.ToString();
+            phase.ReadInCategory = () => category;
+            Assert.That(phase.RollAndPlayAndScore(), Is.EqualTo($"Category: {category} Score: 0"));
+        }
+
+
+        [Test]
+        public void ScoreTwoOnesInOnesCategory()
+        {
+            var phase = new Phase();
+            phase.Roll = new List<int> { 1, 1, 2, 3, 2 };
+            var category = ScoreCategory.Ones.ToString();
+            phase.ReadInCategory = () => category;
+            Assert.That(phase.RollAndPlayAndScore(), Is.EqualTo($"Category: {category} Score: 2"));
+        }
+
+
+        [Test]
+        public void ScoreManuallyTypedCategory()
+        {
+            var phase = new Phase();
+            phase.Roll = new List<int> { 1, 1, 2, 3, 2 };
+            var category = "ones";
+            phase.ReadInCategory = () => category;
+            Assert.That(phase.RollAndPlayAndScore(), Is.EqualTo($"Category: {ScoreCategory.Ones} Score: 2"));
+        }
+
+
+        [Test]
+        public void HandleMissingCategory()
+        {
+            var phase = new Phase();
+            phase.Roll = new List<int> { 1, 1, 2, 3, 2 };
+            var category = "";
+            phase.ReadInCategory = () => category;
+            Assert.That(phase.RollAndPlayAndScore(), Is.EqualTo($"error msg"));
         }
 
     }
